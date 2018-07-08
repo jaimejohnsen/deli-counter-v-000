0feca221d78c72ab2ca5df3e3bef5569058de5ea
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-08 02:51:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,10 +13,7 @@ end
 
 
 def take_a_number(katz_deli, name)
-  if katz_deli.length < 1 
-    puts "There is nobody waiting to be served!"
-    
-  katz_deli << name
+    katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
