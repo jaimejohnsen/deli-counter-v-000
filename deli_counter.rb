07960462de075e06be2e17f850d81264be6b1ed7
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-08 02:55:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,11 +18,11 @@ def take_a_number(katz_deli, name)
 end
 
 
-def now_serving
+def now_serving(name)
   if katz_deli.length < 1 
     puts "There is nobody waiting to be served!"
   else next_person = katz_deli.shift
-    
+    next_person << 
   puts "Currently serving #{name}"
 end
 
