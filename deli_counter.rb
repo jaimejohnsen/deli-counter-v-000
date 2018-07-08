a5d51ed2de613d414d9bef5a1579f05606982911
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-08 02:04:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+
+def line(katz_deli)
+  if katz_deli.length < 1 
+    puts "The line is currently empty."
+  else
+  wait_list = []
+  katz_deli.each_with_index do |name, index|
+  wait_list << "#{index + 1}. #{name}"
+end
+  puts "The line is currently: #{wait_list.join(" ")}"
+  end
+end
